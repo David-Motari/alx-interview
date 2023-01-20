54f81218160cb0a5440d8dd6cd331eae926c380b
under-ground-dog <davemotari22@gmail.com>
2023-01-20 16:17:52 +0300

    Correction

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,53 +1,68 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" read stdin line by line and computer metrics
 """
-reads stdin line by line and computes metrics
-"""
-from sys import stdin
+import sys
+import re
+import signal
+from collections import OrderedDict
 
 
-if __name__ == "__main__":
-    stts = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0,
-    }
-    count = 1
+def search_items(line, s):
+    """ search items to position"""
+    regexu = r"\s\d{3}\s\d{1,}"
+    txt = re.search(regexu, line)
+    word = txt.group()
+    word = word[1:]
+
+    regexd = r"\d{3}\s"
+    left = re.search(regexd, word)
+
+    code = left.group()
+    code = code[:-1]
+
+    regext = r"\s\d{1,}"
+    right = re.search(regext, word)
+
+    size = right.group()
+    size = size[1:]
+    size = int(size)
+
+    add_code(code, s)
+
+    return size
+
+def add_code(code, codes):
+    """count the status code"""
+    try:
+        codes[code] += 1
+    except KeyError:
+        pass
+
+def print_all(stat):
+    """ Print all """
+    stat = OrderedDict(stat)
+
+    for key, values in stat.items():
+        if value is not 0:
+            print("{}: {}".format(key, value))
+
+if __name__ == "__main_":
+    status = {"200":0, "301":0, "400":0, "401":0,
+            "403":0, "404":0,"405":0,"500":0}
     file_size = 0
-
-    def get_line(line):
-        """
-        Parse line and grab data from it
-        """
-        try:
-            read_line = line.split()
-            stts_code = read_line[-2]
-            if stts_code in stts.keys():
-                stts[stts_code] += 1
-            return int(read_line[-1])
-        except Exception:
-            return 0
-
-    def print_stats():
-        """
-        print stats
-        """
-        print("File size: {}".format(file_size))
-        for key in sorted(stts.keys()):
-            if stts[key]:
-                print("{}: {}".format(key, stts[key]))
+    i = 0
 
     try:
-        for line in stdin:
-            file_size += get_line(line)
-            if count % 10 == 0:
-                print_stats()
-            count += 1
+        for lines in sys.stdin:
+            file_size += search_items(lines, status)
+
+            if i is not 0 and i % 9 == 0:
+                print("Files size: {:d}".format(file_size))
+                print_all(status)
+
+            i += 1
     except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
+        pass
+    finally:
+        print_all(status)
+        sys.exit(0)
