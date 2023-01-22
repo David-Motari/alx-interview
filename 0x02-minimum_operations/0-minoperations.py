b4e30d38eed3eca819065dc7288cd624a7de6a3f
David_Motari <davemotari22@gmail.com>
2023-01-22 13:16:58 +0300

    Task 0- finding sum of factor of given n

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env pyhton3
+"""
+0-minoperations
+"""
+
+
+def minOperations(n: int) -> int:
+    """
+    finds minimum  number of copy all and paste print n Hs in a file
+    """
+    ops = 0
+    if not isinstance(n, int) or n <= 1:
+        return 0
+    for i in range(2, n + 1):
+        """ """
+        while n % i == 0:
+            n = n / i
+            ops += i
+    return ops
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,2 @@
+# 0x02. Minimum Operations.
+Using factorization to find the minimum number of operations to have n Hs in a file using Copy All and Paste.
